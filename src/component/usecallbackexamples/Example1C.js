5f9c45c08a694d053ca0786dca1d560fa5626243
aamir140896h <aamir140896h@gmail.com>
2025-03-24 15:36:58 +0530

    new changes

--- src/component/usecallbackexamples/Example1C.js
+++ src/component/usecallbackexamples/Example1C.js
@@ -3,6 +3,7 @@ import React from "react";
 const Example1C = React.memo(({ handleClick }) => {
   console.log("child Component");
 
+  console.log("child Component");
   return <button onClick={handleClick}>Click C</button>;
 });
 
